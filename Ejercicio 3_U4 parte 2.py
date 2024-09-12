9b7549c8cf0a1aba0ea0453c5322ac5024825064
Aylu <105893167+Aylu0808@users.noreply.github.com>
2024-09-12 10:48:01 -0300

    Ejercicio 3_U4 parte 2.py

---
+++ Ejercicio 3_U4 parte 2.py
@@ -0,0 +1,36 @@
+"""Ingresar 3 valores A, B, C que corresponden a los coeficientes de una ecuación de
+segundo grado.
+Determinar si se obtiene raíces complejas, mediante un mensaje.
+El proceso termina con el ingreso de 100 ternas de valores o que se ingrese una terna
+de valores nulos """
+
+CONT = 0
+raiz = 0
+raiz1 = 0
+raiz2 = 0
+
+while(CONT < 100):
+    
+    A = int(input("Ingrese el coeficiente A: "))
+    B = int(input("Ingrese el coeficiente B: "))
+    C = int(input("Ingrese el coeficiente C: "))
+    
+    if(A == 0 and C == 0):
+        if(B == 0):
+            print("Todos los coeficientes son 0.")
+            break
+        
+    raiz = B**2 - 4*A*C
+        
+    if(raiz < 0):
+        print("Tiene raices complejas")
+    else:
+        raiz1 = (- B - (B**2 - 4*A*C)**0.5) / (2*A)
+        raiz2 = (- B - (B**2 - 4*A*C)**0.5) / (2*A)
+            
+        print("Las raices son: ",raiz1,"y",raiz2)
+        
+if(CONT == 100):
+    print("Se llego a las 100 ternas")
+        
+            

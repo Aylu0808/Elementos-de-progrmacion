7bc963bca29c54892b4cd5c79e0a6daa678e62e0
Aylu <105893167+Aylu0808@users.noreply.github.com>
2024-09-12 11:17:00 -0300

    Ejercicio 3_U4 parte 2.py

--- Ejercicio 3_U4 parte 2.py
+++ Ejercicio 3_U4 parte 2.py
@@ -8,9 +8,9 @@ CONT = 0
 raiz = 0
 raiz1 = 0
 raiz2 = 0
-flag = 0
+SUM = 0
 
-while(CONT < 100):
+while(CONT <=99):
     
     A = int(input("Ingrese el coeficiente A: "))
     B = int(input("Ingrese el coeficiente B: "))
@@ -19,15 +19,16 @@ while(CONT < 100):
     if(A == 0 and C == 0):
         if(B == 0):
             print("Todos los coeficientes son 0.")
-            CONT = 100
-            flag = 0
-            
+            CONT = 99
+    
+    SUM = A + B + C
+
     if((B**2 - 4*A*C) < 0):
         print("Tiene raices complejas")
+        CONT = CONT + 1
     elif((B**2 - 4*A*C) > 0):
         print("Tiene raices reales")
-
-
-if(CONT == 100 and flag == 1):
-    print("Se llego a las 100 ternas")
-            
+        CONT = CONT + 1
+        
+    if(CONT == 100 and SUM != 0):
+        print("Se llego a las 100 ternas")

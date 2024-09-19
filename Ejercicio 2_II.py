55e8a8ce5ec7d43b3786a5e385d4caee3d522ba0
Aylu <105893167+Aylu0808@users.noreply.github.com>
2024-09-19 11:55:00 -0300

    Create Ejercicio 2_II.py
    FALTAN LAS VARIABLES

---
+++ Ejercicio 2_II.py
@@ -0,0 +1,76 @@
+"""Se ingresan datos de los empleados de una empresa. Por cada empleado se
+ingresa:
+ Legajo (entero entre 1000 y 5000) CONTROLAR
+ Sueldo básico (float mayor a 1000) CONTROLAR
+ Antigüedad en años (mayor o igual a 0) CONTROLAR
+ Sexo (‘M' o 'F') CONTROLAR
+ Categoría (entero entre 1 a 5) CONTROLAR
+Por cada empleado ingresado se debe calcular el sueldo final a abonar sabiendo
+que:
+Las Categorías 2 y 3 tienen $500 de bonificación.
+La Categoría 4 tiene 10% de bonificación.
+La Categoría 5 tiene 30% de bonificación.
+Si la antigüedad es mayor a 10 años recibe una bonificación del 10% adicional.
+Todos los datos ingresados deben ser validados. El ingreso finaliza con un legajo
+igual a cero.
+Informar:
+a- El sueldo a abonar a cada empleado.
+b- Cantidad de empleados de más de 10 años de antigüedad.
+c- El mayor sueldo y el legajo del empleado que cobra dicho sueldo.
+d- Cantidad de hombres y de mujeres"""
+
+legajo = int(input("Ingrese el numero de legajo: "))
+while legajo < 1000 and legajo > 5000:
+    legajo = int(input("Ingrese un numero de legajo valido: "))
+    
+while legajo != 0:
+    
+    sueldo = float(input("Ingrese el sueldo basico: "))
+    while sueldo < 1000:
+        sueldo = float(input("ERROR!! Ingrese un sueldo valido: "))
+        
+    if flag == 0:
+        mayor = sueldo
+        empleado = legajo
+    else:
+        if mayor < sueldo:
+            empleado = legajo
+            mayor = sueldo
+    
+    antiguedad = int(input("Ingrese los años de antiguedad: "))
+    while antiguedad < 0:
+        antiguedad = int(print("ERROR!!Ingrese de nuevo los años de antiguedad: "))
+        
+    if antiguedad > 10:
+        bonificacion = sueldo * 0.90
+        total = sueldo + bonificacion
+        CANT = CANT + 1
+        
+    sexo = input("Ingrese el sexo del empleado: ")
+    while sexo != 'M' or sexo != 'F':
+        sexo = input("ERROR!! Ingrese un sexo valido: ")
+        
+    if sexo == 'M':
+        M = M +1
+    else:
+        F = F + 1
+        
+    categoria = int(input("Ingrese la categoriadel empleado: "))
+    while categoria < 1 and categoria > 5:
+        categoria = int(input("ERROR!! Ingrese una categoria valida: "))
+        
+    if categoria == 2 or categoria == 3:
+        total = sueldo + 500
+        print("El sueldo a abonar es: ",total)
+    if categoria == 4:
+        bonificacion = sueldo * 0.90
+        total = bonificacion + sueldo
+        print("El sueldo a abonar es: ",total)
+    if categoria == 5:
+        bonificacion = sueldo * 0.70
+        total = bonificacion + sueldo
+        print("El sueldo a abonar es: ",total)
+        
+print("La cantidad total de mujeres es: ",F,"y la cantidad total de hombres es: ",M)
+print("El empleado con mayor sueldo es el de: ",empleado,"con un sueldo de: ",mayor)
+print("La cantidad de empleados con mas de 10 años de antiguedad es: ",CANT)

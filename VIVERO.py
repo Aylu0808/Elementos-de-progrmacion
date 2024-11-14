326d8aaf14f8a578f6a4fc0cbe999760b8e42f51
Aylu <105893167+Aylu0808@users.noreply.github.com>
2024-11-14 10:29:20 -0300

    Update VIVERO.py

--- VIVERO.py
+++ VIVERO.py
@@ -1,38 +1,49 @@
 class Vivero:
-    def __init__(self):
-        self.cant_alarmaspar = [0] * 4
-        self.cant_alarmasimpar = [0] * 4
-        self.medicionespar = [0] * 4
-        self.medicionesimpar = [0] * 4
-        self.mediciones_porhora = [0] * 24
-#SETTERS
-def set_medicionespar(self, indice,valor):
-    if 0 <= indice < len(self.medicionespar):
-        self.medicionespar[indice] = valor
-
-def set_medicionesimpar(self,indice,valor):
-    if 0 <= indice < len(self.medicionesimpar):
-        self.medicionesimpar[indice] = valor
-
-def set_cantpar(self,indice,valor):
-    if 0 <= indice < len(self.cant_alarmaspar):
-        self.cant_alarmaspar[indice] = valor
-
-def set_cantimpar(self,indice,valor):
-    if 0 <= indice < len(self.cant_alarmasimpar):
-        self.cant_alarmasimpar[indice] = valor
+    def __init__(self): #ATRIBUTOS
+        self.cont_alarmaspar = 0
+        self.cont_alarmasimpar = 0
+        self.cant_mediciones = [0] * 8
+        self.caNt_medicioneshora = [0] * 24
+        self.acum_promediopar = 0
+        self.acum_promedioimpar = 0
+        self.alcance_prompar = 0 #No tiene set porque no lo envian es un calculo
+        self.alcance_promimpar = 0
+ #SETTERS       
+    def set_cantalarmaspar(self):
+        self.cont_alarmaspar + = 1
+        
+    def set_cantalarmasimpar(self):
+        self.cont_alarmasimpar += 1
+        
+    def set_mediciones(self,POS): #PASA VALORES DEL MAIN.
+        self.cant_medicione[POS] += 1
+        
+    def set_medicionesporhora(self,POS):
+        self.cant_medicioneshora[POS] += 1
+    
+    def set_acumpar(self,valor):
+        self.acum_promediopar += valor
+        
+    def set_acumimpar(self,valor):
+        self.acum_promedioimpar += valor
 #GETTERS
-def get_medicionespar(self):
-    return self.medicionespar
+    def get_mediciones(self,POS):
+        return self.mediciones[POS]
 
-def get_medicionesimpar(self):
-    return self.medicionesimpar
+    def get_cantalarmaspar(self):
+        return self.cantalarmaspar
+            
+    def get_cantalarmasimpar(self):
+        return self.contalarmasimpar
+            
+    def set_medicionesporhora(self):
+        return self.cant_medicioneshora[POS]
+#PROMEDIO
+    def get_acumpar(self):
+        return self.acum_promediopar // self.cant_alarmaspar
+            
+    def get_acumimpar(self):
+        return self.acum_promedioimpar // self.cant_alarmasimpar
 
-def get_cantpar(self):
-    return self.cant_alarmaspar
+        
 
-def get_catimpar(self):
-    return self.cant_alarmasimpar
-
-def get_mediciones(self):
-    return self.mediciones_porhora

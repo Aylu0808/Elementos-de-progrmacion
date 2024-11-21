b763c47ec85a68c287f257e581af22d0d14fc292
Aylu <105893167+Aylu0808@users.noreply.github.com>
2024-11-21 18:30:45 -0300

    Actualizar Principal.py

--- Principal.py
+++ Principal.py
@@ -1,7 +1,7 @@
 import random
 import machine
 import time
-from vivero import vivero
+from vivero import Vivero
 #Configuracion de los pines
 # Pines para los segmentos del display de 7 segmentos
 pin_A = machine.Pin(2, machine.Pin.OUT)
